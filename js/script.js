d1085774b58e3362cf0b90448057efbfe1789c39
Engy Ahmed Abdelaziz <engya306@gmail.com>
2023-12-26 14:38:08 +0200

    Lab1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//question 1
+var firstName = prompt("Enter your first name");
+var lastName = prompt("Enter your last name");
+var fullName = firstName + " " + lastName;
+confirm("Your full name is " + fullName);
+var birthYear = prompt("Enter your birth year");
+var age = 2023 - birthYear;
+alert("Welcome " + fullName + " you are " + age + " years old");
+
+//question 2
+alert("Welcome to the calculator app\nit’s the first release of a calculator that only has a summation feature");
+var firstNumber = prompt("Enter the first number");
+var secondNumber = prompt("Enter the second number");
+var sum = Number(firstNumber) + Number(secondNumber);
+alert("The sum of " + firstNumber + " and " + secondNumber + " is " + sum);
+
+
+
